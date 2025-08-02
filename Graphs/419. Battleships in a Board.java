a4ba540afd53c8d6b331fee307688dc5e52300fd
Naik Pragni <129032980+Pragni24@users.noreply.github.com>
2025-08-02 13:23:55 +0530

    Update 419. Battleships in a Board.java

--- Graphs/419. Battleships in a Board.java
+++ Graphs/419. Battleships in a Board.java
@@ -5,9 +5,7 @@ class Solution {
         }
         board[i][j]='.';
         dfs(board,i+1,j);
-        dfs(board,i-1,j);
         dfs(board,i,j+1);
-        dfs(board,i,j-1);
     }
     public int countBattleships(char[][] board) {
         int count=0;
